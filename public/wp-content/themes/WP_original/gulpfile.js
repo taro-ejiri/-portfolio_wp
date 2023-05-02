b614773a564385f33c0030d6d9d2a05c28b136cb
taro-ejiri <sept1976@gmail.com>
2023-05-02 15:19:19 +0900

    wordpress_themes　screenshot.png

--- public/wp-content/themes/WP_original/gulpfile.js
+++ public/wp-content/themes/WP_original/gulpfile.js
@@ -1,28 +1,22 @@
 /*
-src : 参照元を指定
-dest : 出力先を指定
-watch : ファイル監視
-series (直列処理) と parallel (並列処理)
+src 参照元を指定
+dest 出力先を指定
+watch ファイル監視
+series(直列処理) と parallel(並列処理)
 */
 
 // gulpプラグインの読み込み
 const { src, dest, watch } = require("gulp");
-
 // Sassをコンパイルするプラグインの読み込み
 const sass = require("gulp-sass")(require("sass"));
-
 // ベンダープレフィックス自動付与
 const autoprefixer = require('gulp-autoprefixer');
-
 // css-mqpackerを使うために必要
 const postcss = require("gulp-postcss");
-
 // メディアクエリをまとめる
 const mqpacker = require('css-mqpacker');
-
 // メディアクエリをソート
 const sortCSSmq = require("sort-css-media-queries");
-
 // プロパティ順をソート
 const cssdeclsort = require("css-declaration-sorter"); 
 
@@ -38,33 +32,23 @@ const TARGET_BROWSERS = [
  * Sassをコンパイルするタスク
  */
 const compileSass = () => {
-    //コンパイル元 style.scssファイルを取得
-    return src("sass/**/*.scss") 
+    return src("sass/**/*.scss") //コンパイル元 style.scssファイルを取得
+        .pipe(sass({outputStyle: "compressed"})) //Sassのコンパイルを実行 コンパイル後のCSSを展開
+        // 指定できるキー expanded(css見やすく) compressed(全CSSコードが1行になる)      
+        .pipe(autoprefixer(TARGET_BROWSERS)) //ベンダープレフィックス
 
-    // Sassのコンパイルを実行 コンパイル後のCSSを展開
-    .pipe(sass({outputStyle: "compressed"})) 
-    // 指定できるキー 
-    // expanded (cssファイルが見やすくなる) 
-    // compressed (CSSがminifyされる)
+        // sassの後に指定
+        .pipe(postcss([cssdeclsort({order: "concentric-css"})])) // concentric-cssでソートする（ボックスモデルの外側からソートする）
+        .pipe(postcss([mqpacker({sort: sortCSSmq.desktopFirst})]))
 
-    //ベンダープレフィックス
-    .pipe(autoprefixer(TARGET_BROWSERS))
-
-    // sassの後に指定
-    // concentric-cssでソートする（ボックスモデルの外側からソートする）
-    .pipe(postcss([cssdeclsort({order: "concentric-css"})]))
-    .pipe(postcss([mqpacker({sort: sortCSSmq.desktopFirst})]))
-
-    //メディアクエリをまとめる
-    .pipe(postcss([mqpacker()]))
-        //cssフォルダー以下に保存 コンパイル先  
-    .pipe(dest("css"));
-}
+        .pipe(postcss([mqpacker()])) //メディアクエリをまとめる
+        .pipe(dest("css")); //cssフォルダー以下に保存 コンパイル先     
+    }
     
 /**
- * Sassファイルを監視し、変更があったらファイルを自動的に変換します
+ * Sassファイルを監視し、変更があったらSassを変換しaます
  */
 const watchSassFiles = () => watch("sass/**/*.scss", compileSass);
 
-// npx gulp または gulp コマンドを実行した時、watchSassFiles(上記の自動変換)が実行される
+// npx gulpというコマンドを実行した時、watchSassFilesが実行される
 exports.default = watchSassFiles;
